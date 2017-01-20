a365057cf47405b54af331e30a3c612527a086b3
Victor Aluko <victor.aluko@gmail.com>
2017-01-20 04:19:41 +0200

    Added angularjs and the controller to git

---
+++ js/orders_controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Victor on 20/01/2017.
+ */
+
+var cashadApp = angular.module('cashad', []);
+
+
+cashadApp.controller('OrdersCtrl', function ($scope) {
+    $scope.title = "Orders Managemt";
+});
