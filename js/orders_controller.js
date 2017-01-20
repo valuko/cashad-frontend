200317ca208ba025fad094f39abe258e2d5ce547
Victor Aluko <victor.aluko@gmail.com>
2017-01-20 04:36:43 +0200

    Switched to use bower for loading the components

--- js/orders_controller.js
+++ js/orders_controller.js
@@ -2,9 +2,20 @@
  * Created by Victor on 20/01/2017.
  */
 
-var cashadApp = angular.module('cashad', []);
+var cashadApp = angular.module('cashad', ['ngResource']);
 
+cashadApp.service('UserService', function () {
 
-cashadApp.controller('OrdersCtrl', function ($scope) {
+})
+
+.service('ProductService', function () {
+
+})
+
+.service('OrderService', function () {
+
+})
+
+.controller('OrdersCtrl', function ($scope) {
     $scope.title = "Orders Managemt";
 });
